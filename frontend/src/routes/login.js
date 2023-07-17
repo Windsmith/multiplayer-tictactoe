9430b3ff866dabc4e2ca66eb982ea671927ff590
Windsmith <shriramsekar11@gmail.com>
2023-07-17 13:14:07 +0400

    feat: navigate to game page after login/signup

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { VStack, Text, Box, FormControl, FormLabel, Input, FormHelperText, Button } from "@chakra-ui/react";
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
 
     const login = async () => {
         const response = await fetch('/user/login', {
@@ -17,8 +19,11 @@ export default function Login() {
                 password
             })
         })
-        console.log(await response.json())
+        //TODO: add error handling when login doesn't return a token
+
         //TODO: add token saving to context and redirection
+        navigate('/game')
+
     }
 
     return (
--- frontend/src/routes/signup.js
+++ frontend/src/routes/signup.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { VStack, Text, Box, FormControl, FormLabel, Input, FormHelperText, Button } from "@chakra-ui/react";
 
 export default function Signup() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
 
     const signup = async () => {
         const response = await fetch('/user/signup', {
@@ -19,7 +21,10 @@ export default function Signup() {
                 password
             })
         })
+        //TODO: add error handling when signup doesn't return a token
+
         //TODO: add token saving to context and redirection
+        navigate('/game')
     }
 
     return (
